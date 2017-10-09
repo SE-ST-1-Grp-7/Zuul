5d5393dca949d55a5c4c5ad5ce09e30471dcd20b
Niclas Johansen <niclasjohansen@Niclass-MacBook-Pro.local>
2017-10-09 15:49:32 +0200

    Added constructor to person.class with arguments

--- Semesterprojekt/src/worldofzuul/Person.java
+++ Semesterprojekt/src/worldofzuul/Person.java
@@ -1,5 +1,7 @@
 package worldofzuul;
 
+import java.util.Scanner;
+
 /** Person class - superclass of Student & Player
  *
  * @author Gruppe 7
@@ -8,6 +10,20 @@ public class Person {
     private String personName;
     private int personAge;
     private String personGender;
+    private Inventory inventory;
     
+    /**
+     * One-arg constructor with personName String, personAge int and personGender String.
+     * 
+     * @param personName, personAge and personGender Passed string and int argument providing room description.
+     */
+    public Person(String personName, int personAge, String personGender, Inventory inventory) {
+        this.personName = personName;
+        this.personAge = personAge;
+        this.personGender = personGender;
+        this.inventory = inventory;
+    } 
     
 }
+
+        
