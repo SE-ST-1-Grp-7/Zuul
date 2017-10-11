b6233fd73b2e8534bbf3228b96a424e76401301c
Robinlp <rphjerpsted@gmail.com>
2017-10-11 17:18:39 +0200

    made a constructor that uses the getRandomName method to assign a name

--- Semesterprojekt/src/worldofzuul/Student.java
+++ Semesterprojekt/src/worldofzuul/Student.java
@@ -12,10 +12,29 @@ package worldofzuul;
 public class Student extends Person {
     boolean hasQuestionToPlayer;
     
+    /**
+     * Constructor that gives a random name
+     * @param age
+     * @param gender
+     * @param speed 
+     */
+    public Student(int age, String gender, int speed){
+        hasQuestionToPlayer = true;
+        super.setName(getRandomName());
+        super.setAge(age);
+        super.setGender(gender);
+        super.setSpeed(speed);
+
+    }
+    
     
     /**
      * This constructor makes a student that has a question for the Player
      * the student also has a name, age, gender and speed
+     * @param name
+     * @param age
+     * @param gender
+     * @param speed
      */
     public Student(String name, int age, String gender, int speed){
         hasQuestionToPlayer = true;
@@ -25,12 +44,14 @@ public class Student extends Person {
         super.setSpeed(speed);
     }
      /**
-     * Randomly create a name for the student
+     * Randomly create a (predefined)name for the student
      *
+     * @return 
      */
+    public String getRandomName(){
         String[] names = {"Niclas", "Rasmus", "Søren", "Robin","Jonas","Magnus","Frederik"};
         int index = (int) (Math.random() * names.length);
-        String name = names[index];
-        
+        return names[index];
+    }
        
 }
