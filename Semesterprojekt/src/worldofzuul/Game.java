2f9ff842f09567e987747303425a97c440d1619d
GreyishOne <rovictim@gmail.com>
2017-10-13 12:03:59 +0200

    Update to game
    moving processcommand to separate class

--- Semesterprojekt/src/worldofzuul/Game.java
+++ Semesterprojekt/src/worldofzuul/Game.java
@@ -50,38 +50,6 @@ public class Game {
     }
 
     /**
-     * Method processes user command to action. 
-     * If the user wants to get help printed, go to a different room or quit.
-     *
-     * @param command String argument of user command input.
-     * @return Boolean return true if user wants to quit.
-     */
-    private boolean processCommand(Command command) {
-        boolean wantToQuit = false;
-
-        CommandWord commandWord = command.getCommandWord();
-
-        // If user input is not a defined command.
-        if (commandWord == CommandWord.UNKNOWN) {
-            System.out.println("I don't know what you mean...");
-            return false;
-        }
-
-        // If user input is request for help print.
-        if (commandWord == CommandWord.HELP) {
-            PrintOut.printHelp(parser);
-        } // Or if user prompts for move to another room.
-        else if (commandWord == CommandWord.GO) {
-            rooms.goRoom(command);
-        } // Or if user prompt to quit.
-        else if (commandWord == CommandWord.QUIT) {
-            wantToQuit = quit(command);
-        }
-        // Quit boolean, true if user wants to quit.
-        return wantToQuit;
-    }
-
-    /**
      * Quit handler method. If the user command isn't just quit, when print
      * error message and return the quit boolean as false.
      *
