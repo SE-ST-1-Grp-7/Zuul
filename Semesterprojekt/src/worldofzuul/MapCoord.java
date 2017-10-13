a3fa242432dbd71a968050efd8f2de9274c2ad6a
polse123 <sorenbendtsen1993@gmail.com>
2017-10-13 12:41:05 +0200

    mapCoords
    maps har fået en atribut som hedder koordinater, med setters og getters

---
+++ Semesterprojekt/src/worldofzuul/MapCoord.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author Soren
+ */
+public class MapCoord{
+    private int X;
+    private int Y;
+
+    public MapCoord() {
+        this(0,0);
+    }        
+    public MapCoord(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+    public int getX() {
+        return X;
+    }
+    public int getY() {
+        return Y;
+    }
+    public void setX(int X) {
+        this.X = X;
+    }
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+}
