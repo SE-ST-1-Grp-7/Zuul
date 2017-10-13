c71c1137d8ef354cf565ae6a78b0d3bf3a33be11
0xjb <Jonas_46@msn.com>
2017-10-13 13:26:53 +0200

    please work

--- Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
+++ Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
@@ -1,32 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package worldofzuul.mapAndRooms;
 
 /**
  *
- * @author niclasjohansen
+ * @author Niclas Johansen & Rasmus WIller
  */
 public class Map {
-
     MapCoord mapcords = new MapCoord();
     private int item;
     private int exit;
     private String description;
     private boolean locked;
-<<<<<<< HEAD:Semesterprojekt/src/worldofzuul/Map.java
-    
     
     public Map(){
         
-    }
-
-=======
-
-    public Map() {
-
     }
 
     public int item() {
@@ -43,29 +29,6 @@ public class Map {
 
     public boolean locked() {
         return this.locked;
->>>>>>> Game:Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
+
     }
-//    public int item(){
-//        return this.item;
-//    }
-//    
-//    public int exit(){
-//        return this.exit;
-//    }
-//    
-//    public String description(){
-//        return this.description;
-//    }
-//    
-//    public boolean locked() {
-//        return this.locked;
-//    }
-
 }
-<<<<<<< HEAD:Semesterprojekt/src/worldofzuul/Map.java
-
-
-   
-
-=======
->>>>>>> Game:Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
