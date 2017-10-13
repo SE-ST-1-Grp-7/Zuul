3b33d65c17c4067f14e6e21dc43195db2c332620
Robinlp <rphjerpsted@gmail.com>
2017-10-13 12:08:47 +0200

    removed commented old code

--- Semesterprojekt/src/worldofzuul/People/Person.java
+++ Semesterprojekt/src/worldofzuul/People/Person.java
@@ -53,24 +53,7 @@ public abstract class Person {
      */
     public void setSpeed(int speed) {
         this.speed = speed;
-    }
-
-    
-    
-         
-//    
-//    /**
-//     * One-arg constructor with personName String, personAge int and personGender String.
-//     * 
-//     * @param personName, personAge and personGender Passed string and int argument providing room description.
-//     */
-//    public Person(String personName, int personAge, String personGender, Inventory inventory) {
-//        this.personName = personName;
-//        this.personAge = personAge;
-//        this.personGender = personGender;
-//        this.inventory = inventory;
-//    } 
-    
+    } 
 }
 
         
