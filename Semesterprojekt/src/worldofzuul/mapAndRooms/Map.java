91ecd22cc807361f2275da3aadfa1d37879e4faf
GreyishOne <32192161+GreyishOne@users.noreply.github.com>
2017-10-13 13:22:35 +0200

    fix error in class

--- Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
+++ Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
@@ -10,20 +10,15 @@ package worldofzuul.mapAndRooms;
  * @author niclasjohansen
  */
 public class Map {
-
     MapCoord mapcords = new MapCoord();
     private int item;
     private int exit;
     private String description;
     private boolean locked;
-<<<<<<< HEAD:Semesterprojekt/src/worldofzuul/Map.java
-    
     
     public Map(){
         
     }
-
-=======
 
     public Map() {
 
@@ -43,29 +38,22 @@ public class Map {
 
     public boolean locked() {
         return this.locked;
->>>>>>> Game:Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
+
     }
-//    public int item(){
-//        return this.item;
-//    }
-//    
-//    public int exit(){
-//        return this.exit;
-//    }
-//    
-//    public String description(){
-//        return this.description;
-//    }
-//    
-//    public boolean locked() {
-//        return this.locked;
-//    }
+    public int item(){
+        return this.item;
+    }
+    
+    public int exit(){
+        return this.exit;
+    }
+    
+    public String description(){
+        return this.description;
+    }
+    
+    public boolean locked() {
+        return this.locked;
+    }
 
 }
-<<<<<<< HEAD:Semesterprojekt/src/worldofzuul/Map.java
-
-
-   
-
-=======
->>>>>>> Game:Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
