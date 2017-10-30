fac01b22110125039a959a00c979829683110075
Robinlp <rphjerpsted@gmail.com>
2017-10-30 13:19:51 +0100

    comments in chair

--- Semesterprojekt/src/worldofzuul/Furniture/Chair.java
+++ Semesterprojekt/src/worldofzuul/Furniture/Chair.java
@@ -6,7 +6,7 @@
 package worldofzuul.Furniture;
 
 /**
- *
+ *a subclass of furniture that makes a chair
  * @author Robin
  */
 public class Chair extends Furniture /*implements Interactable*/{
@@ -17,21 +17,21 @@ public class Chair extends Furniture /*implements Interactable*/{
      * @param chairDescription 
      */
     public Chair(String chairName, String chairDescription){
-        super(chairName, chairDescription);
+        super(chairName, chairDescription);//a call to the super constructor - sets the name and description
     }
     
     /**
      * method for sitting in the chair
      */
     public void sitInChair(){
-        
+        //not implementet yet
     }
     
     /**
      * method for standing up again after sitting in the chair
      */
     public void standUp(){
-        
+        //not implementet yet
     }
     
 }
