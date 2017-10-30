f7676ff7cc049abb866ea1fa8f680089c75590c7
Robinlp <rphjerpsted@gmail.com>
2017-10-30 12:55:40 +0100

    comments and javaDoc in person

--- Semesterprojekt/src/worldofzuul/People/Person.java
+++ Semesterprojekt/src/worldofzuul/People/Person.java
@@ -5,21 +5,37 @@ package worldofzuul.People;
  * @author Gruppe 7, Robin & Niclas & soren
  */
 public abstract class Person {
-    private String name;
-    private int speed;
-    private int x,y;
+    private String name; // name for the person
+    private int speed; // speed of the person
+    private int x,y; // x and y coordinates for the person
     
     
+    /**
+     * a constructor that gives a random name
+     * @param speed
+     * @param x
+     * @param y 
+     */
     public Person(int speed, int x, int y){
+        //the other constructor is called with these arguments
         this(getRandomName(), speed, x, y);
     }
     
+    /**
+     * constructor where you can specify the name, speed and position
+     * @param name
+     * @param speed
+     * @param x
+     * @param y 
+     */
     public Person(String name, int speed, int x, int y){
+        //the parameters are set equal to the attributes
         this.name = name;
         this.speed = speed;
         this.x = x;
         this.y = y;
     }
+    
     /**
      * getter for the name
      * @return 
@@ -27,6 +43,7 @@ public abstract class Person {
     public String getName() {
         return this.name;
     }
+    
     /**
      * setter for the name
      * @param name 
@@ -34,6 +51,7 @@ public abstract class Person {
     public void setName(String name) {
         this.name = name;
     }
+    
     /**
      * getter for the speed
      * @return 
@@ -41,6 +59,7 @@ public abstract class Person {
     public int getSpeed() {
         return this.speed;
     }
+    
     /**
      * setter for the speed
      * @param speed 
@@ -48,9 +67,9 @@ public abstract class Person {
     public void setSpeed(int speed) {
         this.speed = speed;
     }
-         /**
+    
+    /**
      * Randomly create a (predefined)name
-     *
      * @return 
      */
     public static String getRandomName(){
@@ -59,18 +78,34 @@ public abstract class Person {
         return names[index];
     }
     
+    /**
+     * getter for x
+     * @return 
+     */
     public int getX(){
         return this.x;
     }
     
+    /**
+     * getter for y
+     * @return 
+     */
     public int getY(){
         return this.y;
     }
     
+    /**
+     * setter for x
+     * @param x 
+     */
     public void setX(int x){
         this.x = x;
     }
     
+    /**
+     * setter for y
+     * @param y 
+     */
     public void setY(int y){
         this.y = y;
     }
