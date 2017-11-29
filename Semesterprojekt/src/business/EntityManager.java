5713860f7b8aebb2919d9147bb2638b1efb116c1
Robinlp <rphjerpsted@gmail.com>
2017-11-29 12:16:26 +0100

    use key to unlock doors

--- Semesterprojekt/src/business/EntityManager.java
+++ Semesterprojekt/src/business/EntityManager.java
@@ -37,7 +37,7 @@ public class EntityManager {
     public EntityManager(RoomManager rm) {
         this.rm = rm;                   // Assign room manager object to class.
         loadPresetEntities();           // Load entity IDs from CSV file.
-        String playerName = "Peter";    // Temp define for player name.
+        String playerName = "Pete";    // Temp define for player name.
         addEntitiesToRooms(playerName); // Instantiate entities defined in CSV.
 
     }
