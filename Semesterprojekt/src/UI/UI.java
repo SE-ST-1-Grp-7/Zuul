43a6f197ff2f6179ccafe5ec4dc6e52aec0f0705
Niclas Johansen <niclas@coweb.dk>
2017-11-30 18:50:29 +0100

    fix

--- Semesterprojekt/src/UI/UI.java
+++ Semesterprojekt/src/UI/UI.java
@@ -34,7 +34,7 @@ public class UI extends Application implements IUI {
         Scene scene = new Scene(p);
         scene.getStylesheets().add("https://fonts.googleapis.com/css?family=Bangers");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
         primaryStage.show();
 
     }
